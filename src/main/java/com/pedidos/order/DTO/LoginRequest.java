2cf8d27fd94d15e44a0e2ea5083713e6834e94fc
WILLIAM GABRIEL ROQUE DE ASSIS <roquew804@gmail.com>
2026-06-10 21:57:17 -0300

    att

--- src/main/java/com/pedidos/order/DTO/LoginRequest.java
+++ src/main/java/com/pedidos/order/DTO/LoginRequest.java
@@ -1,4 +1,22 @@
 package com.pedidos.order.DTO;
 
 public class LoginRequest {
+    private String email;
+    private String senha;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
