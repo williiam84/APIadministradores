bf2b1db3e876c486bc173265322c73afc446c9b6
William <roquew804@gmail.com>
2026-06-10 22:17:36 -0300

    siuuuuu

--- src/main/java/com/pedidos/order/controller/AdminController.java
+++ src/main/java/com/pedidos/order/controller/AdminController.java
@@ -1,6 +1,9 @@
 package com.pedidos.order.controller;
 
+<<<<<<< HEAD
 import com.pedidos.order.bancodedados.BancoAdmin;
+=======
+>>>>>>> 2a15bb3 (salvando alterações antes do pull)
 import com.pedidos.order.dto.LoginRequest;
 import com.pedidos.order.model.Admin;
 import com.pedidos.order.service.AdminService;
@@ -8,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
@@ -32,9 +37,23 @@ public class AdminController {
     }
 
     @PostMapping("/login")
+<<<<<<< HEAD
     public ResponseEntity<?> login(@RequestBody LoginRequest dados){
         return ResponseEntity.ok(
                 adminService.login(dados.getEmail(), dados.getSenha())
+=======
+    public ResponseEntity<Map<String, String>> login(@RequestBody LoginRequest req) {
+
+        String token = adminService.login(req.getEmail(), req.getSenha());
+
+        if (token == null) {
+            return ResponseEntity.status(401)
+                    .body(Map.of("error", "Login inválido"));
+        }
+
+        return ResponseEntity.ok(
+                Map.of("token", token)
+>>>>>>> 2a15bb3 (salvando alterações antes do pull)
         );
     }
 }
